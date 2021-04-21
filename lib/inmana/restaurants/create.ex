d919b10f1db00f733702d236da0aa7cd5eede72f
Rodrigo Goulart Ribeiro <rodrigo.goulart.ribeiro@gmail.com>
2021-04-21 17:32:49 -0300

    Create a restaurant schema module and restaurant create module

---
+++ lib/inmana/restaurants/create.ex
@@ -0,0 +1,16 @@
+defmodule Inmana.Restaurants.Create do
+  @moduledoc """
+  Module to create restaurants on Inmana Repo.
+  """
+  alias Inmana.{Repo, Restaurant}
+
+  def call(params) do
+    params
+    |> Restaurant.changeset()
+    |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %Restaurant{}} = result), do: result
+  defp handle_insert({:error, result}), do: {:error, %{result: result, status: :bad_request}}
+end

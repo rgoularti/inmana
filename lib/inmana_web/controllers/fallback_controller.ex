b6e2dc2dc7860618ae2b30c88ddd8e53716e4411
Rodrigo Goulart Ribeiro <rodrigo.goulart.ribeiro@gmail.com>
2021-04-21 18:17:34 -0300

    Create a fallback controller to handle errors when calling Inmana apis

---
+++ lib/inmana_web/controllers/fallback_controller.ex
@@ -0,0 +1,15 @@
+defmodule InmanaWeb.FallbackController do
+  @moduledoc """
+  Inmana Fallback controller.
+  """
+  use InmanaWeb, :controller
+
+  alias InmanaWeb.ErrorView
+
+  def call(conn, {:error, %{result: result, status: status}}) do
+    conn
+    |> put_status(status)
+    |> put_view(ErrorView)
+    |> render("error.json", result: result)
+  end
+end

6d9bf149cfb6a2661585f899e2effe054c1a640a
Rodrigo Goulart Ribeiro <rodrigo.goulart.ribeiro@gmail.com>
2021-04-22 19:43:19 -0300

    Create supplies table migration

---
+++ priv/repo/migrations/20210422223659_create_supplies_table.exs
@@ -0,0 +1,17 @@
+defmodule Inmana.Repo.Migrations.CreateSuppliesTable do
+  @moduledoc """
+  Create supplies table.
+  """
+  use Ecto.Migration
+
+  def change do
+    create table(:supplies) do
+      add :description, :string
+      add :expiration_date, :date
+      add :responsible, :string
+      add :restaurant_id, references(:restaurants, type: :binary_id)
+
+      timestamps()
+    end
+  end
+end

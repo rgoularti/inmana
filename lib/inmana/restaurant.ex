b3175cd7ff19f33c62a5211cfb1b868f67c15486
Rodrigo Goulart Ribeiro <rodrigo.goulart.ribeiro@gmail.com>
2021-04-22 19:54:13 -0300

    Create supply schema and configure relationship with restaurant schema

--- lib/inmana/restaurant.ex
+++ lib/inmana/restaurant.ex
@@ -5,6 +5,8 @@ defmodule Inmana.Restaurant do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Inmana.Supply
+
   @primary_key {:id, :binary_id, autogenerate: true}
 
   @required_params [:email, :name]
@@ -14,6 +16,8 @@ defmodule Inmana.Restaurant do
   schema "restaurants" do
     field :email, :string
     field :name, :string
+
+    has_many :supplies, Supply
 
     timestamps()
   end

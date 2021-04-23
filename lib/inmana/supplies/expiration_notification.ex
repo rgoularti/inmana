a4557325c3485caff01ed90af1ed7c827ca66c8d
Rodrigo Goulart Ribeiro <rodrigo.goulart.ribeiro@gmail.com>
2021-04-23 18:57:05 -0300

    Refactor the supplies expiration notification module to use async tasks to send the notification emails

--- lib/inmana/supplies/expiration_notification.ex
+++ lib/inmana/supplies/expiration_notification.ex
@@ -8,10 +8,14 @@ defmodule Inmana.Supplies.ExpirationNotification do
   def send do
     data = GetByExpiration.call()
 
-    Enum.each(data, fn {to_email, expired_supplies} ->
-      to_email
-      |> ExpirationEmail.create(expired_supplies)
-      |> Mailer.deliver_later!()
-    end)
+    data
+    |> Task.async_stream(fn {to_email, supplies} -> send_email(to_email, supplies) end)
+    |> Stream.run()
+  end
+
+  defp send_email(to_email, supplies) do
+    to_email
+    |> ExpirationEmail.create(supplies)
+    |> Mailer.deliver_later!()
   end
 end
